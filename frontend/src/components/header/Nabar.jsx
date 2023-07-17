d9e24530018b237f6c16e3d9694fc8dbe850d238
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-17 11:26:04 +0500

    developing branch created

--- frontend/src/components/header/Nabar.jsx
+++ frontend/src/components/header/Nabar.jsx
@@ -1,54 +1,54 @@
 import React, { useState } from 'react'
-import {Link} from "react-scroll"
+import { Link } from "react-scroll"
 import { FaBars, FaTimes } from "react-icons/fa"
 const NavBar = () => {
     const [nav, setNav] = useState(false);
     // navigations
     const navBar = [
         {
+            id: 0,
+            name: "Home",
+            href: "home"
+        },
+        {
             id: 1,
-           name: "Home",
-           href: "home"
+            name: "About us",
+            href: "about"
         },
         {
             id: 2,
-           name: "About",
-           href: "about"
+            name: "Portfolio",
+            href: "portfolio"
         },
         {
             id: 3,
-           name: "Portfolio",
-           href: "portfolio"
+            name: "Experience",
+            href: "experience"
         },
         {
             id: 4,
-           name: "Experience",
-           href: "experience"
-        },
-        {
-            id: 5,
-           name: "Contact",
-           href: "contact"
+            name: "Contact",
+            href: "contact"
         }
     ]
     return (
-        <div className='flex justify-between items-center px-4 w-full h-20 text-white bg-gradient-to-t from-gray-900 bg-black fixed'>
+        <div className='flex justify-between items-center px-4 w-full h-28 text-white bg-gradient-to-t from-gray-900 bg-black fixed'>
             <div>
-                <h1 className='text-5xl font-signature ml-2'>wasim</h1>
+                <h1 className='text-5xl font-signature ml-2'>HungerCare</h1>
             </div>
             <ul className='hidden md:flex'>
                 {
-                    navBar.map(({ id, name ,href }) => (
-                        <li  className='px-4 cursor-pointer capitalize font-medium
+                    navBar.map(({ id, name, href }) => (
+                        <li className='px-4 cursor-pointer capitalize font-medium
                         hover:scale-125  duration-200 hover:text-rose-600 hover:underline' key={id}>
-                            <Link 
-                              to={href}
-                              activeClass="active"
-                              spy={true}
-                              smooth={true}
-                              duration={400}
-                              offset={-80}
-                              className="transition-all duration-400"
+                            <Link
+                                to={href}
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                duration={400}
+                                offset={-80}
+                                className="transition-all duration-400"
                             >
                                 {name}
                             </Link>
@@ -67,21 +67,21 @@ const NavBar = () => {
                 <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full
                  h-screen bg-gradient-to-b from-black to-gray-800 text-gray-300 '>
                     {
-                        navBar.map(({ id, name , href }) => (
-                            <li className='px-4 cursor-pointer capitalize py-6 text-4xl'  key={id} >
-                               <Link 
-                              to={href}
-                              activeClass="active"
-                              spy={true}
-                              smooth={true}
-                              duration={400}
-                              offset={-80}
-                              className="transition-all duration-400"
-                              onClick={() => setNav(false)}
-                            >
-                                {name}
-                            </Link>  
-                                </li>
+                        navBar.map(({ id, name, href }) => (
+                            <li className='px-4 cursor-pointer capitalize py-6 text-4xl' key={id} >
+                                <Link
+                                    to={href}
+                                    activeClass="active"
+                                    spy={true}
+                                    smooth={true}
+                                    duration={400}
+                                    offset={-80}
+                                    className="transition-all duration-400"
+                                    onClick={() => setNav(false)}
+                                >
+                                    {name}
+                                </Link>
+                            </li>
                         ))
                     }
                 </ul>
