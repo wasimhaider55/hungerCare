2acb3c79f8fdc457b0b2c696af6783ca3cbfe451
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-18 17:00:18 +0500

    added navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
-import NavBar from "./components/header/Nabar";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import NavBar from "./components/navbar/Nabar";
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter >
+      <Header />    
       <NavBar />
-    </div>
+      <Home/>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className=' text-white bg-red-800 mt-24'>home</div>
+
+    </div>
+  )
+}
+
+export default Home
