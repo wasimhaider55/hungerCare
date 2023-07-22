a2a99c12dfde3a5d06389f3ee8fda22920e2daf1
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-22 13:12:09 +0500

    about us page is ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-// import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 // import Home from "./components/home/Home"
 import AboutUs from "./components/about/AboutUs";
 
 function App() {
   return (
     <div >  
-      {/* <Navbar/>
-      <Home/> */}
+      <Navbar/>
+      {/* <Home/> */}
       <AboutUs/>
 
     </div>
--- frontend/src/components/about/AboutUs.jsx
+++ frontend/src/components/about/AboutUs.jsx
@@ -3,20 +3,25 @@ import React from 'react'
 
 const AboutUs = () => {
     return (
-        <section>
-            <div className=' w-full h-52 bg-gray-700 bg-cover bg-center bg-[url("https://image.khaleejtimes.com/?uuid=9d530cb1-b2d2-5933-b804-fabeb18997de&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.85188&x=0&y=0&width=1200&height=675")]'
-            >
-                <div className=' text-white text-4xl font-bold  pt-20 pl-20 '>
+        <main className='pt-20'>
+
+            <div className=' w-full h-52 bg-gray-700  bg-cover bg-center bg-[url("https://image.khaleejtimes.com/?uuid=9d530cb1-b2d2-5933-b804-fabeb18997de&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.85188&x=0&y=0&width=1200&height=675")]'>
+                <div className=' text-white text-4xl font-bold  pt-20 md:pl-20 pl-10 '>
                     <h1 className=''> About Us </h1>
-                    <hr className='w-24' />
+                    <div className=' w-24 h-1 bg-red-500 rounded-full mt-3 '></div>
                 </div>
             </div>
 
-            <h1 className=' text-4xl font-bold pt-24 px-24  '>Who WE Are</h1>
 
-            <div className=' flex  pt-10 mb-24  px-24 gap-8'>
-                <div className="w-[50%]">
-                    <p>
+            {/* who we are section */}
+            <div className=' lg:text-4xl text-3xl font-bold pt-24 lg:pl-24 md:pl-20 pl-4 uppercase'>
+                <h1>Who WE Are</h1>
+                <div className=' w-20 h-1 bg-blue-600 rounded-full mt-3 '></div>
+            </div>
+
+            <div className='lg:flex lg:pl-24  pt-10 mb-24 px-4 md:px-20  gap-6'>
+                <div className="lg:w-[50%]">
+                    <p className=' pl-1 pb-4 lg:pr-24 md:pr-24 sm:pr-24 text-justify '>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
@@ -25,12 +30,70 @@ const AboutUs = () => {
                     </p>
                 </div>
 
-                <div className='w-[50%]'>
+                <div className='lg:w-[50%]'>
                     <img src="https://love.lambeth.gov.uk/wp-content/uploads/2020/05/hubs-2-500-x-320.jpg" alt="who we are " />
                 </div>
             </div>
 
-        </section>
+
+            {/* Our history section  */}
+            <section className=' bg-black text-white'>
+            <div className=' lg:text-4xl text-3xl font-bold pt-16 lg:pl-24 md:pl-20 pl-4 uppercase'>
+                <h1>Our History</h1>
+                <div className=' w-16 h-1 bg-blue-600 rounded-full mt-3 '></div>
+            </div>
+
+                <div className='lg:flex lg:pl-24  pt-10 mb-24 px-4 md:px-20  gap-6'>
+                    <div className="lg:w-[50%]">
+                        <p className=' pl-1 pb-4 lg:pr-24 md:pr-24 sm:pr-24 text-justify '>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum,
+                        </p>
+                    </div>
+
+                    <div className='lg:w-[50%]'>
+                        <img src="https://love.lambeth.gov.uk/wp-content/uploads/2020/05/hubs-2-500-x-320.jpg" alt="who we are " />
+                    </div>
+                </div>
+
+            </section>
+
+
+
+
+            {/* what we Do section  */}
+
+            <section>
+            <div className=' lg:text-4xl text-3xl font-bold pt-4 lg:pl-24 md:pl-20 pl-4 uppercase'>
+                <h1>What We Do</h1>
+                <div className=' w-20 h-1 bg-blue-600 rounded-full mt-3 '></div>
+            </div>
+
+                <div className='lg:flex lg:pl-24  pt-10 mb-24 px-4 md:px-20  gap-6'>
+                    <div className="lg:w-[50%]">
+                        <p className=' pl-1 pb-4 lg:pr-24 md:pr-24 sm:pr-24 text-justify '>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum,
+                        </p>
+                    </div>
+
+                    <div className='lg:w-[50%]'>
+                        <img src="https://love.lambeth.gov.uk/wp-content/uploads/2020/05/hubs-2-500-x-320.jpg" alt="who we are " />
+                    </div>
+                </div>
+            </section>
+
+
+        </main>
+
+
+
     )
 }
 
