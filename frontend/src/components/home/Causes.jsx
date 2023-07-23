07ff7d377375144a7d07cd9e3099c62f8b06efcd
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-23 23:17:10 +0500

    install package multi cursel in react and aslo make a causes components responsive

--- frontend/src/components/home/Causes.jsx
+++ frontend/src/components/home/Causes.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 const Causes = () => {
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
     return (
         <section>
 
@@ -10,45 +31,71 @@ const Causes = () => {
                 Our Causes
             </h1>
 
-
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 mx-3 sm:mx-20 sm:px-0 py-24 text-white '>
-
-                <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
-                    <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
-                    <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
-                    <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
-                        aspernatur vitae, officia deserunt facilis suscipit
-                    </p>
+            <Carousel responsive={responsive} className='py-10 lg:mx-10'>
+                <div className=' lg:mx-16 md:mx-12 sm:mx-10 mx-5'>
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
                 </div>
-
-
-                <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
-                    <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
-                    <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
-                    <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
-                        aspernatur vitae, officia deserunt facilis suscipit
-                    </p>
+                <div className=' lg:mx-16 md:mx-12 sm:mx-10 mx-5' >
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
                 </div>
-
-
-                <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
-                    <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
-                    <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
-                    <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
-                        aspernatur vitae, officia deserunt facilis suscipit
-                    </p>
+                <div className=' lg:mx-16 md:mx-12 sm:mx-10 mx-5'>
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
                 </div>
-
-            </div>
-
-            {/* left Arrow */}
-            <div>
-                <BsChevronCompactLeft size={130} />
-            </div>
-            {/* right Arrow */}
-            <div>
-                <BsChevronCompactRight size={130} />
-            </div>
+                <div className='mx-4'>
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
+                </div>
+                <div className='mx-4'>
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
+                </div>
+                <div className='mx-4'>
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
+                </div>
+                <div className='mx-4'>
+                    <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                        <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" />
+                        <h1 className=' px-4 text-md pt-8'>Free Food Distribution</h1>
+                        <p className=' px-4 text-sm leading-7 my-10 font-light opacity-50 pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                            aspernatur vitae, officia deserunt facilis suscipit
+                        </p>
+                    </div>
+                </div>
+            </Carousel>;
         </section>
     )
 }
