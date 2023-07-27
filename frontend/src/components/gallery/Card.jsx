11f674e660d39dd9367de57264d3d94d489b495c
Abdurazaq333 <irazaqr986@gmail.com>
2023-07-27 04:43:35 -0700

    Some modification in gallery card

--- frontend/src/components/gallery/Card.jsx
+++ frontend/src/components/gallery/Card.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  const { title, image } = props;
   return (
-    <div className="w-full ">
+    <div className="max-w-xs rounded overflow-hidden shadow-lg ">
       <div className="bg-gray-200 p-2 border-2 border-gray-500 ">
-        <img src="https://image.khaleejtimes.com/?uuid=9d530cb1-b2d2-5933-b804-fabeb18997de&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.85188&x=0&y=0&width=1200&height=675" />
-        <p className="py-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, modi!
-        </p>
+        <img src={image} alt="title" />
+        <p className="py-5">{title}</p>
       </div>
     </div>
   );
