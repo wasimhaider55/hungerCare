d9cec2f0a1e951982a02f898a1ace7ca01b1a9fc
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-27 22:48:22 +0500

    free food distribution componets is completly ready

--- frontend/src/components/navbar/pages/freefooddist/DailyMealDis.jsx
+++ frontend/src/components/navbar/pages/freefooddist/DailyMealDis.jsx
@@ -189,6 +189,175 @@ const DailyMealDis = () => {
             </section>
             {/*Monthly Ration Packages section closed */}
 
+
+            {/*Ramazan Ration Packages section open*/}
+            <section className='bg-gradient-to-t from-blue-900 bg-black text-white'>
+
+                <div className=' lg:text-4xl text-3xl font-bold pt-16 lg:pl-24 md:pl-20  uppercase'>
+                    <h1 className='sm:pl-8 '>3. Ramazan RATION PACKAGES</h1>
+                </div>
+
+                <div className='lg:flex lg:pl-24  mb-24 px-4 md:px-20'>
+
+                    <div className="lg:w-[60%] pt-10">
+                        <p className='sm:pl-8  pb-4 xl:pr-5 lg:pr-24 md:pr-14 sm:pr-24 text-justify'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum,
+                        </p>
+                    </div>
+
+                    <div className='lg:w-[40%] pt-20'>
+                        <div className=' lg:mx-16 md:mx-10 sm:mx-10 rounded-md shadow-lg shadow-slate-900'>
+                            <div className=' bg-gradient-to-l from-black to-blue-700  rounded-md cursor-pointer text-white'>
+                                <img src={bg1} alt="deily lunch distribution" className='p-1' />
+                                <h1 className=' px-4 text-md pt-4 text-center'>Picture Gallery</h1>
+                                <p className=' px-4 text-sm my-3 font-light pb-4 text-center '>
+                                    Ramzan Ration Packeges Distribution
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Carousel responsive={responsive} className=' lg:mx-20 text-white'>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                </Carousel>
+            </section>
+            {/*Ramazan Ration Packages section closed*/}
+
+
+            {/*Ramazan Iftaar dastarkhwan section open*/}
+            <section className=' bg-white text-black'>
+
+                <div className=' lg:text-4xl text-3xl font-bold pt-16 lg:pl-24 md:pl-20  uppercase'>
+                    <h1 className='sm:pl-8 uppercase'>4.Ramazan Iftaar dastarkhwan</h1>
+                </div>
+
+                <div className='lg:flex lg:pl-24  mb-24 px-4 md:px-20'>
+
+                    <div className="lg:w-[60%] pt-10">
+                        <p className='sm:pl-8  pb-4 xl:pr-5 lg:pr-24 md:pr-14 sm:pr-24 text-justify'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+                            Lorem ipsum,
+                        </p>
+                    </div>
+
+                    <div className='lg:w-[40%] pt-20'>
+                        <div className=' lg:mx-16 md:mx-10 sm:mx-10 rounded-md shadow-lg shadow-slate-900'>
+                            <div className=' bg-gradient-to-l from-black to-blue-700  rounded-md cursor-pointer text-white'>
+                                <img src={bg1} alt="deily lunch distribution" className='p-1' />
+                                <h1 className=' px-4 text-md pt-4 text-center'>Picture Gallery</h1>
+                                <p className=' px-4 text-sm my-3 font-light pb-4 text-center '>
+                                    Ramazan Iftaar dastarkhwan
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Carousel responsive={responsive} className=' lg:mx-20 text-white'>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                    <div className='lg:mx-3 md:mx-12 sm:mx-10 mx-5'>
+                        <div className=' bg-gradient-to-l from-black to-blue-700  rounded-xl cursor-pointer'>
+                            <img src="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4" alt="free food distribution" className='p-1' />
+                            <p className=' px-4 text-md pt-10  my-10 font-light  pb-4 '>Lorem, ipsum dolor sit amet consectetur adipisicing
+                                aspernatur vitae, officia deserunt facilis suscipit
+                            </p>
+                        </div>
+                    </div>
+                </Carousel>
+            </section>
+            {/*Ramazan Iftaar dastarkhwan section closed*/}
+
+
+            {/* Help us Section open */}
+            <div className='md:flex md:justify-around md:items-center  py-10 px-14 h-56 bg-blue-400 '>
+                <h1 className='text-white font-bold sm:text-3xl text-2xl'>Help Us So We Can Kill The Hunger</h1>
+                <button className=' border-2 border-white rounded-2xl p-3 ml-24 sm:ml-40 md:mt-0 mt-10 text-white font-bold hover:bg-blue-500'>Donate Now </button>
+            </div>
+            {/* Help us Section closed */}
+
+
+
+
         </main>
     )
 }
