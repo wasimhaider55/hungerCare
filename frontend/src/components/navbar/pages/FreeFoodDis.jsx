364960c0e3c431a2a5966161f74c9a642bcdfa75
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-27 11:41:28 +0500

    worked on free fodd dis

--- frontend/src/components/navbar/pages/FreeFoodDis.jsx
+++ frontend/src/components/navbar/pages/FreeFoodDis.jsx
@@ -2,22 +2,34 @@ import React from "react";
 
 const FreeFoodDis = () => {
   return (
-    <div>
-      <h1>thus us free fiid</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius illum esse
-        deleniti temporibus quidem nulla consectetur nemo perspiciatis adipisci
-        ipsum, reiciendis id quas ut expedita, eum, voluptate ipsa libero odio.
-        Error est officiis nam. Impedit dolorem qui sunt hic et cumque
-        repudiandae quas dolor animi distinctio tempora labore quidem voluptatum
-        voluptates ratione beatae quos numquam nam libero nesciunt vitae odio
-        esse, maiores quo. Consequatur, doloribus saepe esse accusantium sed vel
-        nihil dolore sint! At vero odio nobis! Labore vel veniam tempora non
-        beatae maxime voluptas sequi suscipit deserunt. Expedita consequatur
-        atque, reprehenderit consequuntur velit voluptatem nihil dolor nesciunt
-        alias ratione!
-      </p>
-    </div>
+    <main className=" pt-20">
+      {/*Free Food Dis section open */}
+      <div className='w-full h-52 bg-cover bg-center bg-[url("https://image.khaleejtimes.com/?uuid=9d530cb1-b2d2-5933-b804-fabeb18997de&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.85188&x=0&y=0&width=1200&height=675")]'>
+        <div className=' text-white text-4xl font-bold  pt-20 md:pl-20 pl-10 '>
+          <h1 className=''>Free Food Distribution </h1>
+          <div className=' w-24 h-1 bg-red-500 rounded-full mt-3 '></div>
+        </div>
+      </div>
+      {/* Free Food Dis section Closed */}
+
+
+      {/* Overview section Open */}
+      <div className=' lg:text-4xl text-3xl font-bold py-16 lg:pl-24 md:pl-20 pl-4'>
+        <h1>Overview</h1>
+        <div className=' w-20 h-1 bg-blue-600 rounded-full mt-3 '></div>
+        <p className=' pl-1 pr-4 py-8 lg:pr-24 md:pr-24 sm:pr-24 text-justify text-lg '>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid ipsa totam dolore quam, eligendi distinctio consectetur voluptatibus maxime? Expedita eaque, nulla ipsa et blanditiis alias tenetur nemo repellat voluptas!
+          Lorem ipsum,
+        </p>
+      </div>
+      {/* Overview section Closed */}
+
+      
+
+    </main>
   );
 };
 
