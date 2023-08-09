2fdaaf2696541693bde01af70ed75238c05cc36a
Abdurazaq333 <irazaqr986@gmail.com>
2023-08-09 03:39:55 -0700

    card color change

--- frontend/src/components/gallery/pictureGallery/Card.jsx
+++ frontend/src/components/gallery/pictureGallery/Card.jsx
@@ -6,18 +6,18 @@ const Card = (props) => {
   return (
     <div className="w-100   rounded-lg overflow-hidden  transition transform hover:-translate-y-1 shadow-lg shadow-slate-900 ">
       <Link to={link}>
-        <div className="bg-blue-600  border-2 border-gray-500 ">
+        <div className="bg-gray-100  border-2 border-gray-500 ">
           <div className="relative group">
             <img
               src={image}
               alt="title"
               className="w-full h-50 object-cover "
             />
-            <button className="mx-[8rem] my-[5rem] opacity-0 absolute inset-0 flex justify-center items-center transition duration-300 bg-white border border-blue-500 text-blue-500 text-sm font-semibold py-2 px-4 rounded-full group-hover:opacity-100">
+            <button className="mx-[8rem] my-[5rem] opacity-0 absolute inset-0 flex justify-center items-center transition duration-300 bg-white border border-gray-500 text-gray-500 text-sm font-semibold py-2 px-4 rounded-full group-hover:opacity-100">
               ViewMore
             </button>
           </div>
-          <p className="py-5 text-xl  text-white  text-center">{title}</p>
+          <p className="py-5 text-xl  text-black  text-center">{title}</p>
         </div>
       </Link>
     </div>
--- frontend/src/components/navbar/pages/education/Card.jsx
+++ frontend/src/components/navbar/pages/education/Card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
   return (
     <Link to="/gallery/picturegallery">
       <div className=" lg:mx-16 md:mx-10 sm:mx-10 rounded-md shadow-lg shadow-slate-900">
-        <div className=" bg-blue-700  rounded-md cursor-pointer text-white">
+        <div className=" bg-gray-100  rounded-md cursor-pointer text-black">
           <img src={RRPD} alt="deily lunch distribution" className="p-1" />
           <h1 className=" px-4 text-md pt-4 text-center">Picture Gallery</h1>
           <p className=" px-4 text-sm my-3 font-light pb-4 text-center ">
