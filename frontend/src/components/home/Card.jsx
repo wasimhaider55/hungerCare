78945d2020551c20e1eeda8cdb7cf79014cc22a6
wasim Haider55 <wasimhaider14379@gmail.com>
2023-08-12 14:54:42 +0500

    some changing

--- frontend/src/components/home/Card.jsx
+++ frontend/src/components/home/Card.jsx
@@ -23,8 +23,8 @@ const Card = () => {
                     <div className='bg-gradient-to-b from-[#8ebf9b] via-[#f3f7f5] hover:bg-gradient-to-t hover:from-blue-400 hover:to-blue-400 text-black hover:text-white p-8 rounded-xl mix-blend-luminosity cursor-pointer shadow-lg shadow-slate-900 hover:scale-105'>
                         <span className='grid grid-cols-1 place-items-center'> <BiDonateHeart size={50} /> </span>
                         <h1 className=' capitalize text-xl font-bold text-center'>what we do</h1>
-                        <p className=' text-sm leading-7 my-3 font-light  '>Lorem, ipsum dolor sit amet consectetur adipisicing
-                            aspernatur vitae, officia deserunt facilis suscipit
+                        <p className=' text-sm leading-7 my-3 font-light '>
+                            We provide cash, clothing, food, and medicine to the poorest areas of district Hangu.
                         </p>
                     </div>
                 </Link>
@@ -33,8 +33,8 @@ const Card = () => {
                     <div className='bg-gradient-to-b from-blue-400 via-[#d8f6e7]  hover:bg-gradient-to-t hover:from-blue-400 hover:to-blue-400 text-black hover:text-white p-8 rounded-xl mix-blend-luminosity cursor-pointer shadow-lg shadow-slate-900 hover:scale-105'>
                         <span className='grid grid-cols-1 place-items-center'> <RiTeamLine size={50} /> </span>
                         <h1 className=' capitalize text-xl font-bold text-center'>Our Team</h1>
-                        <p className=' text-sm leading-7 my-3 font-light'>Lorem, ipsum dolor sit amet consectetur adipisicing
-                            aspernatur vitae, officia deserunt facilis suscipit
+                        <p className=' text-sm leading-7 my-3 font-light'>
+                            Our volunteers and staff work tirelessly to provide community services
                         </p>
                     </div>
                 </Link>
