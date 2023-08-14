a45c9222bcc2e2106589cdad0ec6c329f91e1562
wasim Haider55 <wasimhaider14379@gmail.com>
2023-08-14 16:24:51 +0500

    added picture to causes card

--- frontend/src/components/home/Causes.jsx
+++ frontend/src/components/home/Causes.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import CausesCard from "./CausesCard";
+import DPS from "../../assets/freeFoodDis/DPS.png"
+import PFS from "../../assets/freeFoodDis/PFS.png"
+import OFS from "../../assets/freeFoodDis/OFS.png"
+import ES from "../../assets/freeFoodDis/ES.png"
 
 const Causes = () => {
   const responsive = {
@@ -35,27 +39,26 @@ const Causes = () => {
           description="We deliver free food to 400+ poor people every day in Hangu District, in addition to monthly ration packages to 100+ needy families."
         />
         <CausesCard
-          image="https://images.squarespace-cdn.com/content/v1/577eab6d03596e5318b18a43/1599066636360-H7Y593575V2IJG569ITW/Orphans.png"
+          image={OFS}
           title="Orphan Family Support"
           description="Numerous orphan families receive monthly financial support to meet
           their basic requirements, such as food and education."
         />
         <CausesCard
-          image="https://images.squarespace-cdn.com/content/v1/577eab6d03596e5318b18a43/1616705131281-N5S4XIVBSLFSPZ4R819V/Orphange-Care.png?format=1500w"
+          image={PFS}
           title="Poor Family support"
           description=" Many poor families are given assistance to meet their daily
           necessities, such as food, clothing, money, and education help."
         />
         <CausesCard
-          image="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4"
+          image={DPS}
           title="Disabled People Support"
           description="  Disabled persons are helped in a variety of ways, including
           monthly cash assistance, wheelchair donations, and other forms of
           assistance."
         />
         <CausesCard
-          image="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4"
-          title="Education Support"
+          image={ES}
           description="   Education is being empowered by compassionate support. Join us in
           our mission to empower students by offering resources and
           opportunities for better futures."
