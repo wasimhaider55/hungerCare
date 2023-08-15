0c1286361ef17f94a3382e597f3cb8561a88aec6
wasim Haider55 <wasimhaider14379@gmail.com>
2023-08-15 15:17:48 +0500

    causes section is completed and fill with real data

--- frontend/src/components/home/Causes.jsx
+++ frontend/src/components/home/Causes.jsx
@@ -6,6 +6,7 @@ import DPS from "../../assets/freeFoodDis/DPS.png"
 import PFS from "../../assets/freeFoodDis/PFS.png"
 import OFS from "../../assets/freeFoodDis/OFS.png"
 import ES from "../../assets/freeFoodDis/ES.png"
+import FFD from "../../assets/freeFoodDis/FFD.jpg"
 
 const Causes = () => {
   const responsive = {
@@ -34,7 +35,7 @@ const Causes = () => {
       </h1>
       <Carousel responsive={responsive} className="py-14 lg:mx-5 text-black">
         <CausesCard
-          image="https://media.licdn.com/dms/image/C4E12AQHY1jqipj8HFA/article-cover_image-shrink_720_1280/0/1644916369002?e=2147483647&v=beta&t=Bpzh_gYu30BcBwkNmh8Ul1Bll91qJ3rKg9aiC1dNEo4"
+          image={FFD}
           title="Free Food Distribution"
           description="We deliver free food to 400+ poor people every day in Hangu District, in addition to monthly ration packages to 100+ needy families."
         />
