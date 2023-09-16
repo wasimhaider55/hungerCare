92d5af21306886233ef05f2e2440fc5389c774ec
Abdurazaq333 <irazaqr986@gmail.com>
2023-09-16 10:40:53 -0700

    picture and album added to gallery

---
+++ frontend/src/components/gallery/pictureGallery/ofsAlbum/ImageObject.jsx
@@ -0,0 +1,53 @@
+import pic1 from "../../../../assets/poorfamilysupport/pic1.jpg";
+import pic2 from "../../../../assets/poorfamilysupport/pic2.jpg";
+import pic3 from "../../../../assets/poorfamilysupport/pic3.jpeg";
+import pic4 from "../../../../assets/poorfamilysupport/pic4.jpeg";
+import pic5 from "../../../../assets/poorfamilysupport/pic5.jpg";
+import pic6 from "../../../../assets/poorfamilysupport/pic6.jpg";
+import pic7 from "../../../../assets/poorfamilysupport/pic7.jpeg";
+import pic8 from "../../../../assets/poorfamilysupport/pic8.jpg";
+import pic9 from "../../../../assets/poorfamilysupport/pic9.jpg";
+import pic10 from "../../../../assets/poorfamilysupport/pic10.jpg";
+
+export const FRD = [
+  {
+    id: 1,
+    img: pic1,
+  },
+  {
+    id: 2,
+    img: pic2,
+  },
+  {
+    id: 3,
+    img: pic3,
+  },
+  {
+    id: 4,
+    img: pic4,
+  },
+  {
+    id: 5,
+    img: pic5,
+  },
+  {
+    id: 6,
+    img: pic6,
+  },
+  {
+    id: 7,
+    img: pic7,
+  },
+  {
+    id: 8,
+    img: pic8,
+  },
+  {
+    id: 9,
+    img: pic9,
+  },
+  {
+    id: 10,
+    img: pic10,
+  },
+];
