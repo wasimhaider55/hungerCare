e5bad1739fb35f4758126cdb04315599706bc8e4
Abdurazaq333 <irazaqr986@gmail.com>
2023-09-17 09:16:32 -0700

    picture added to gallery almost complete gallery

--- frontend/src/components/gallery/pictureGallery/esAlbum/ImageObject.jsx
+++ frontend/src/components/gallery/pictureGallery/esAlbum/ImageObject.jsx
@@ -1,53 +1,53 @@
-import ffd4 from "../../../../assets/freeFoodDis/ffd4.png";
-import ffd3 from "../../../../assets/freeFoodDis/ffd2.png";
-import spbg2 from "../../../../assets/freeFoodDis/spbg2.png";
-import bg1 from "../../../../assets/bg1.jpg";
-import ffd from "../../../../assets/freeFoodDis/FFD.jpg";
-import ffd12 from "../../../../assets/freeFoodDis/ffd12.png";
-import ffd13 from "../../../../assets/freeFoodDis/ffd13.png";
-import ffd14 from "../../../../assets/freeFoodDis/ffd14.png";
-import ffd15 from "../../../../assets/freeFoodDis/ffd15.png";
-import ffd16 from "../../../../assets/freeFoodDis/ffd16.png";
+import pic1 from "../../../../assets/EducationSupport/pic1.png";
+import pic2 from "../../../../assets/EducationSupport/pic2.png";
+import pic3 from "../../../../assets/EducationSupport/pic3.png";
+import pic4 from "../../../../assets/EducationSupport/pic4.png";
+import pic5 from "../../../../assets/EducationSupport/pic5.png";
+import pic6 from "../../../../assets/EducationSupport/pic6.png";
+import pic7 from "../../../../assets/EducationSupport/pic7.png";
+import pic8 from "../../../../assets/EducationSupport/pic8.png";
+import pic9 from "../../../../assets/EducationSupport/pic9.png";
+import pic10 from "../../../../assets/EducationSupport/pic10.png";
 
 export const FRD = [
   {
     id: 1,
-    img: ffd4,
+    img: pic1,
   },
   {
     id: 2,
-    img: ffd3,
+    img: pic2,
   },
   {
     id: 3,
-    img: spbg2,
+    img: pic3,
   },
   {
     id: 4,
-    img: bg1,
+    img: pic4,
   },
   {
     id: 5,
-    img: ffd,
+    img: pic5,
   },
   {
     id: 6,
-    img: ffd12,
+    img: pic6,
   },
   {
     id: 7,
-    img: ffd13,
+    img: pic7,
   },
   {
     id: 8,
-    img: ffd14,
+    img: pic8,
   },
   {
     id: 9,
-    img: ffd15,
+    img: pic9,
   },
   {
     id: 10,
-    img: ffd16,
+    img: pic10,
   },
 ];
