c15ef11ccc03771e3f5d104b94fcfcbe63385115
Abdurazaq333 <irazaqr986@gmail.com>
2023-09-19 10:28:47 -0700

    remove manifest error and responsive impact card

--- frontend/src/components/home/OurImpact.jsx
+++ frontend/src/components/home/OurImpact.jsx
@@ -6,7 +6,7 @@ const OurImpact = () => {
   return (
     <main className="mb-20  mx-12">
       <h1 className="text-4xl font-bold text-center mb-8">Our Impact</h1>
-      <div className="  grid md:grid-cols-4 gap-6 ">
+      <div className="  grid sm:grid-cols-2 lg:grid-cols-4 gap-6 ">
         {Impact.map((item, indx) => {
           return <ImpactCard item={item} key={indx} />;
         })}
