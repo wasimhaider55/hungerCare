01624948bb5d030c1c776a11e84bcb254eca8280
wasim Haider55 <wasimhaider14379@gmail.com>
2023-11-20 09:43:40 +0500

    some small changing

--- frontend/src/components/sideBar/SideBar.jsx
+++ frontend/src/components/sideBar/SideBar.jsx
@@ -1,79 +1,82 @@
 import React, { useState } from "react";
-import { PiBowlFoodBold } from "react-icons/pi";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { RiMessage2Line } from "react-icons/ri";
 import { GiReceiveMoney } from "react-icons/gi";
-import { FaTimes, FaBars } from "react-icons/fa";
+import { PiBowlFoodBold } from "react-icons/pi"
 import { Link } from "react-router-dom";
 
 
 const SideBar = () => {
     const [nav, setNav] = useState(false);
-    const handleClick = () => {
-        setNav(!nav);
-    };
+
     return (
         <>
-            <div onClick={handleClick} className="hidden sm:block z-10">
-                {!nav ? <FaBars size={30} /> : <ul
-                    className={
-                        nav && "hidden sm:block absolute left-0 w-[24rem] h-96  top-0  bg-[#004d73] rounded-br-3xl text-gray-300 py-5"
-                    }
-                >
-                    <h1 className="px-4 cursor-pointer capitalize py-4 text-xl font-semibold">
-                        Sopgmai Foundation
-                    </h1>
-                    <FaTimes size={30} className=" absolute top-10 right-10" />
 
-                    <Link to="/foodMessages" className="flex">
-                        <PiBowlFoodBold size={30} className="ml-5" />
-                        <li
-                            onClick={() => {
-                                setNav(!nav);
-                            }}
-                            activeclass="active"
-                            spy={"true"}
-                            smooth={"true"}
-                            duration={400}
-                            className="px-4 cursor-pointer capitalize pb-4 text-xl font-semibold"
-                        >
-                            Food
-                        </li>
-                    </Link>
+            <div onClick={() => setNav(!nav)} className='hidden lg:block cursor-pointer'>
+                <AiOutlineMenu size={40} />
+            </div>
 
-                    <Link to="/messages" className="flex">
-                        <RiMessage2Line size={30} className="ml-5" />
-                        <li
-                            onClick={() => {
-                                setNav(!nav);
-                            }}
-                            activeclass="active"
-                            spy={"true"}
-                            smooth={"true"}
-                            duration={400}
-                            className=" px-4 cursor-pointer capitalize pb-4 text-xl font-semibold"
-                        >
-                            message
-                        </li>
-                    </Link>
+            <div className={nav ? "hidden lg:block absolute left-0 w-[24rem] h-96  top-0  bg-[#004d73] rounded-br-3xl text-gray-300 py-5" : " fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300 "}>
+                <AiOutlineClose size={30}
+                    onClick={() => setNav(!nav)}
+                    className='absolute right-4 top-10 cursor-pointer '
+                />
+                <h2 className='text-2xl p-4'>
+                    Spogmai Foundation
+                </h2>
+                <div>
+                    <ul className=' flex flex-col p-4 text-white'>
+                        <Link to="/foodMessages" className="flex">
+                            <PiBowlFoodBold size={30} className="ml-5" />
+                            <li
+                                onClick={() => {
+                                    setNav(!nav);
+                                }}
+                                activeclass="active"
+                                spy={"true"}
+                                smooth={"true"}
+                                duration={400}
+                                className="px-4 cursor-pointer capitalize pb-4 text-xl font-semibold"
+                            >
+                                Food
+                            </li>
+                        </Link>
 
-                    <Link className="flex ">
-                        <GiReceiveMoney size={30} className="ml-5"/>
-                        <li
-                            onClick={() => {
-                                setNav(!nav);
-                            }}
-                            activeclass="active"
-                            spy={"true"}
-                            smooth={"true"}
-                            duration={400}
-                            className=" px-4 cursor-pointer capitalize pb-4 text-xl font-semibold"
-                        >
-                            Money
-                        </li>
-                    </Link>
+                        <Link to="/messages" className="flex">
+                            <RiMessage2Line size={30} className="ml-5" />
+                            <li
+                                onClick={() => {
+                                    setNav(!nav);
+                                }}
+                                activeclass="active"
+                                spy={"true"}
+                                smooth={"true"}
+                                duration={400}
+                                className=" px-4 cursor-pointer capitalize pb-4 text-xl font-semibold"
+                            >
+                                message
+                            </li>
+                        </Link>
 
-                </ul>
-                }
+                        <Link className="flex ">
+                            <GiReceiveMoney size={30} className="ml-5" />
+                            <li
+                                onClick={() => {
+                                    setNav(!nav);
+                                }}
+                                activeclass="active"
+                                spy={"true"}
+                                smooth={"true"}
+                                duration={400}
+                                className=" px-4 cursor-pointer capitalize pb-4 text-xl font-semibold"
+                            >
+                                Money
+                            </li>
+                        </Link>
+
+                    </ul>
+                </div>
+
             </div>
         </>
     )
